6f5c525724fc5e361e27818816da6bd3e0411575
Eugene Kozlov <nekoeuge@gmail.com>
2021-03-04 01:11:16 +0300

    Generalize SceneProcessor. Refactoring.

--- Source/Urho3D/SystemUI/DebugHud.cpp
+++ Source/Urho3D/SystemUI/DebugHud.cpp
@@ -160,8 +160,6 @@ void DebugHud::RenderUI(DebugHudModeFlags mode)
 
         float left_offset = ui::GetCursorPos().x;
 
-        ui::Text("[%s]", graphics->GetApiName().c_str());
-        ui::SetCursorPosX(left_offset);
         ui::Text("FPS %d", fps_);
         ui::SetCursorPosX(left_offset);
         ui::Text("Triangles %u", primitives);
@@ -189,7 +187,8 @@ void DebugHud::RenderUI(DebugHudModeFlags mode)
         const ImGuiStyle& style = ui::GetStyle();
         const ImGuiContext& g = *ui::GetCurrentContext();
         ui::SetCursorPos({style.WindowPadding.x, ui::GetWindowSize().y - ui::GetStyle().WindowPadding.y - g.Font->FontSize});
-        ui::Text("Tex:%s | Mat:%s | Spec:%s | Shadows:%s | Size:%i | Quality:%s | Occlusion:%s | Instancing:%s | API:%s",
+        ui::Text("API:%s | Tex:%s | Mat:%s | Spec:%s | Shadows:%s | Size:%i | Quality:%s | Occlusion:%s | Instancing:%s",
+            graphics->GetApiName().c_str(),
             qualityTexts[renderer->GetTextureQuality()],
             qualityTexts[renderer->GetMaterialQuality()],
             renderer->GetSpecularLighting() ? "On" : "Off",
@@ -197,8 +196,7 @@ void DebugHud::RenderUI(DebugHudModeFlags mode)
             renderer->GetShadowMapSize(),
             shadowQualityTexts[renderer->GetShadowQuality()],
             renderer->GetMaxOccluderTriangles() > 0 ? "On" : "Off",
-            renderer->GetDynamicInstancing() ? "On" : "Off",
-            graphics->GetApiName().c_str());
+            renderer->GetDynamicInstancing() ? "On" : "Off");
     }
 }
 

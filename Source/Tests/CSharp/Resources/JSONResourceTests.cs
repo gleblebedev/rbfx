7e57d86fb53a33a6c4a26b5fdac1521ee0af056c
Gleb <gleb@gleblebedev.com>
2024-05-02 21:23:12 +0100

    Tests

--- Source/Tests/CSharp/Resources/JSONResourceTests.cs
+++ Source/Tests/CSharp/Resources/JSONResourceTests.cs
@@ -7,8 +7,25 @@ using Xunit;
 
 namespace Urho3DNet.Tests
 {
+    public enum TestEnum
+    {
+        A,B,C
+    }
+    public class TestContainer
+    {
+        public Vector2 Vector2 { get; set; }
+        public Vector3 Vector3 { get; set; }
+        public Vector4 Vector4 { get; set; }
+        public Quaternion Quaternion { get; set; }
+        public IntVector2 IntVector2 { get; set; }
+        public IntVector3 IntVector3 { get; set; }
+        public BoundingBox BoundingBox { get; set; }
+        public TestEnum Enum { get; set; }
+        public Material Material { get; set; }
+    }
+
     [ObjectFactory]
-    public class TestResource : JSONResource<int>
+    public class TestResource : JSONResource<TestContainer>
     {
         public TestResource(Context context) : base(context)
         {
@@ -20,13 +37,38 @@ namespace Urho3DNet.Tests
         public async Task SaveSimpleResource()
         {
             await RbfxTestFramework.Context.ToMainThreadAsync();
+
+            var mat = new Material(RbfxTestFramework.Context);
+            mat.Name = "TestResource.material";
+            RbfxTestFramework.Context.ResourceCache.AddManualResource(mat);
+
             var res = new TestResource(RbfxTestFramework.Context);
-            res.Value = 42;
+            var testContainer = new TestContainer()
+            {
+                Vector2 = new Vector2(1,2),
+                Vector3 = new Vector3(1,2,3),
+                Vector4 = new Vector4(1,2,3,4),
+                Quaternion = new Quaternion(10, Vector3.Down),
+                IntVector2 = new IntVector2(1, 2),
+                IntVector3 = new IntVector3(1, 2, 3),
+                BoundingBox = new BoundingBox(new Vector3(-1,-2,-3), new Vector3(1, 2, 3)),
+                Enum = TestEnum.B,
+                Material = mat,
+            };
+            res.Value = testContainer;
             var fileIdentifier = new FileIdentifier("conf", "SaveJson.json");
             res.SaveFile(fileIdentifier);
-            res.Value = 1;
+            res.Value = null;
             res.LoadFile(fileIdentifier);
-            Assert.Equal(res.Value, 42);
+            Assert.Equal(testContainer.IntVector2, res.Value.IntVector2);
+            Assert.Equal(testContainer.IntVector3, res.Value.IntVector3);
+            Assert.Equal(testContainer.Vector2, res.Value.Vector2);
+            Assert.Equal(testContainer.Vector3, res.Value.Vector3);
+            Assert.Equal(testContainer.Vector4, res.Value.Vector4);
+            Assert.Equal(testContainer.Quaternion, res.Value.Quaternion);
+            Assert.Equal(testContainer.BoundingBox, res.Value.BoundingBox);
+            Assert.Equal(testContainer.Enum, res.Value.Enum);
+            Assert.Equal(testContainer.Material, res.Value.Material);
             var path = RbfxTestFramework.Context.VirtualFileSystem.GetAbsoluteNameFromIdentifier(fileIdentifier);
             System.IO.File.Delete(path);
         }
